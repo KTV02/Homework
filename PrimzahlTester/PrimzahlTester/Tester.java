7d1c1723c0eef5bd05a9d179d2038f35e4e88ae1
Lennart Kremp <l.kremp@gmx.de>
2019-02-06 18:36:21 +0100

    added print to file for prime

--- PrimzahlTester/PrimzahlTester/Tester.java
+++ PrimzahlTester/PrimzahlTester/Tester.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 /**
  * Beschreiben Sie hier die Klasse Tester.
  * 
@@ -7,24 +8,37 @@ import java.util.*;
  */
 public class Tester
 {
-  public void primzahl(int p){
-      int wurzelp = (int) Math.sqrt(p); 
-      int[] array = new int[wurzelp];
-      boolean s = false;
-      for(int i=0; i<Math.sqrt(p); i++){
-          array[i]=i;
+    public void primzahl(int p){
+        int wurzelp = (int) Math.sqrt(p); 
+        int[] array = new int[wurzelp];
+        boolean s = false;
+        for(int i=0; i<Math.sqrt(p); i++){
+            array[i]=i;
         }
-      for (int x: array){
-          if(p%x==0){
-              s = true;
+        for (int x: array){
+            if(p%x==0){
+                s = true;
             }
-          else{
-              s = false;
+            else{
+                s = false;
             }
         }
         if(s==true)
-          System.out.println("Ja, " + p + " ist eine Primzahl.");
-          else
-          System.out.println("Nein, " + p + " ist keine Primzahl.");
+            System.out.println("Ja, " + p + " ist eine Primzahl.");
+        else
+            System.out.println("Nein, " + p + " ist keine Primzahl.");
+    }
+
+    public void printZahlen(int range){
+        BufferedWriter writer = new BufferedWriter(new FileWriter("samplefile1.txt"));
+        String f="Primzahlen bis "+range+" : ";
+        for(int i=0;i<=range;i++){
+            if(primzahl(i))
+                f=f+i+"; \n";
+
+        }
+        writer.write(f);
+        writer.close();
     }
 }
+
