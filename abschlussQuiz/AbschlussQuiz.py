ba9a8aa90eb9cbfac186f9c4bc465069dea8453e
Lennart Kremp <l.kremp@gmx.de>
2019-04-15 22:27:44 +0200

    finals version abschluss quiz pyton

--- abschlussQuiz/AbschlussQuiz.py
+++ abschlussQuiz/AbschlussQuiz.py
@@ -6,11 +6,6 @@ def leseDatei():
     datei.close()
     return text
 
-
-
-def zufaelligeAufgabe():
-    rand = randint(1, 15)
-
 def frageStellen(combined):
     processed=[]
     processed=combined.split("!")
@@ -36,32 +31,44 @@ def quizStarten():
     spielerCounter=0
     spielerKonto = [500 for i in range(len(spieler))]
     questions=leseDatei().splitlines()
+
+    #loops the questions
     while verloren==False:
-            print("Kontostände: ")
-            for x in range(len(spieler)):
-                print(spieler[x]," : ",spielerKonto[x])
+         #checks if spieler counter greater than number of players
             if spielerCounter<len(spieler):
-                rand = randint(0, len(questions))
+                 #prints balance of every player
+                print("Kontostände: ")
+                for x in range(len(spieler)):
+                    print(spieler[x]," : ",spielerKonto[x])  
+                rand = randint(0, len(questions)-1)
                 print(spieler[spielerCounter], "du bist dran")
+                #asks question
                 beantwortet=frageStellen(questions[rand])
+                  #if answer correct and player not millionaire
                 if beantwortet==True and not spielerKonto[spielerCounter]==1024000:
-                        print("Das hast du ganz toll gemacht")
-                        spielerKonto[spielerCounter]*=2
+                       print("Das hast du ganz toll gemacht")
+                       spielerKonto[spielerCounter]*=2
+                   #if answer incorrect
                 elif beantwortet==False:
                         print("Das ist leider falsch ",spieler[spielerCounter]," scheidet aus")
                         spielerKonto[spielerCounter]=500
                         spieler.pop(spielerCounter)
                         spielerKonto.pop(spielerCounter)
                         spielerCounter-=1
-                elif [spielerCounter]==1024000:
-                        print("Herzlichen Glückwunsch ",spieler[spielerCounter]," Du hast Wer wird 1024000 gewonnen")
-                        exit(0)
-                else:
-                    print("Alle haben Verloren -Die Welt liegt in Trümmern")
-                    verloren=True
+                            
+                    #if player millionaire
+                elif spielerKonto[spielerCounter]==1024000:
+                    print("Herzlichen Glückwunsch ",spieler[spielerCounter]," Du hast Wer wird 1024000 gewonnen")
                     exit(0)
+            #all players lost       
+            elif len(spieler)==0:
+               print("Alle haben Verloren -Die Welt liegt in Trümmern")
+               verloren=True
+               exit(0)
+            #resets spieler counter
             else:
                 spielerCounter=-1
+                
             spielerCounter+=1
    
         
